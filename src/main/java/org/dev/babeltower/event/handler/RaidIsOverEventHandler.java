0eebbbb92182399dbf82f729b9bbd38ea4c29e86
alscjf1329 <alscjf1329@gmail.com>
2024-02-29 14:35:30 +0900

    feat) 바벨탑 레이드에 성공시 해당 레이드의 보상을 mail로 보내주는 로직 추가

--- src/main/java/org/dev/babeltower/event/handler/RaidIsOverEventHandler.java
+++ src/main/java/org/dev/babeltower/event/handler/RaidIsOverEventHandler.java
@@ -1,11 +1,15 @@
 package org.dev.babeltower.event.handler;
 
+import com.hj.rpgsharp.rpg.apis.rpgsharp.utils.Serializer;
+import com.hj.rpgsharp.rpg.plugins.mailbox.objects.Mail;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.Inventory;
+import org.dev.babeltower.BabelTower;
 import org.dev.babeltower.dto.RaidResultDTO;
 import org.dev.babeltower.event.RaidIsOverEvent;
 import org.dev.babeltower.managers.RaidManager;
@@ -22,9 +26,15 @@ public class RaidIsOverEventHandler implements Listener {
 
         int floor = raidResult.getRaid().getTower().getFloor();
         if (raidResult.isSucceeded()) {
+            String serializedReward = raidResult.getRaid().getTower().getSerializedReward();
+            Inventory inventory = Serializer.deserializeInventory(serializedReward);
             long clearTime = raidResult.getClearTime();
             long seconds = TimeUnit.MILLISECONDS.toSeconds(clearTime);
-            ChatView.SUCCESS_RAID.sendTo(player, player.getName(), floor, seconds);
+
+            String message = String.format(ChatView.SUCCESS_RAID.getMessageFormat(),
+                player.getName(), floor, seconds);
+            Mail mail = Mail.createMail(BabelTower.NAME, message, 0.0, inventory.getContents());
+            mail.receive(player);
         } else {
             ChatView.FAIL_RAID.sendTo(player, player.getName(), floor);
         }
