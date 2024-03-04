0c5ee5182b31acf88b7d1cad054f908c02763892
alscjf1329 <alscjf1329@gmail.com>
2024-03-04 14:11:00 +0900

    fix) 레이드 이용하는 유저가 레이드에서 나가도 실패한 레이드 결과가 반영되지 않는 버그 발생
    RaidIsOverEvent를 발생시켜서 버그 해결했습니다.

--- src/main/java/org/dev/babeltower/event/handler/RaidUserQuitEventHandler.java
+++ src/main/java/org/dev/babeltower/event/handler/RaidUserQuitEventHandler.java
@@ -1,18 +1,27 @@
 package org.dev.babeltower.event.handler;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.dev.babeltower.dto.Raid;
+import org.dev.babeltower.dto.RaidResultDTO;
+import org.dev.babeltower.event.RaidIsOverEvent;
 import org.dev.babeltower.managers.RaidManager;
-import org.dev.babeltower.managers.TowerRoomManager;
 
 public class RaidUserQuitEventHandler implements Listener {
 
     @EventHandler
     public void onRaidUserQuit(PlayerQuitEvent playerQuitEvent) {
         Player player = playerQuitEvent.getPlayer();
-        RaidManager.getInstance().clearRaidBy(player);
-        TowerRoomManager.getInstance().unregister(player);
+        Raid raid = RaidManager.getInstance().findRaidBy(player);
+        if (raid == null) {
+            return;
+        }
+        RaidResultDTO failedRaidResult = RaidResultDTO.createFailedRaidResultDTO(
+            raid);
+        Bukkit.getServer().getPluginManager()
+            .callEvent(new RaidIsOverEvent(failedRaidResult));
     }
 }
